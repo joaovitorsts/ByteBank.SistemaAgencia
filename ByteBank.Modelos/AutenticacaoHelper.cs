c82dc130aa4f36fb242dff20d3d410f83e56a091
João Vitor dos Santos <jsantos@isolutions.com.br>
2022-05-04 14:04:02 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.Modelos/AutenticacaoHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    class AutenticacaoHelper
+    {
+        public bool ComparaSenhas(string senha, string senhaTentativa) {
+            return senha == senhaTentativa;
+        }
+    }
+}
